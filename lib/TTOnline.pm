5be9dde44d74c41c69302c911a5c26b86ded6360
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-08-13 23:51:49 -0600

    Add and remove variables from session->('tt')

--- lib/TTOnline.pm
+++ lib/TTOnline.pm
@@ -7,4 +7,19 @@ get '/' => sub {
     template 'index';
 };
 
+post '/' => sub {
+  my $name = param('name');
+  my $value = param('value');
+  my $action = param('action');
+
+  my $tt = session('tt') || {};
+  if( $action eq 'add' ){
+    $tt->{$name} = $value;
+  }
+  elsif( $action eq 'del' ){
+    delete $tt->{$name};
+  }
+  session->write('tt',$tt);
+  redirect "/";
+};
 true;
